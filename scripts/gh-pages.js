db3a645896426e5ff098d71938ecaf5c68e09a01
mvkump <mvkump@utu.fi>
2020-12-03 17:18:59 +0200

    migrated build to root directory

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,10 +1,10 @@
 const ghpages = require("gh-pages");
 
 ghpages.publish(
-  "__sapper__/export/mvkump.dev",
+  "__sapper__/export",
   {
     branch: "master",
-    repo: "https://github.com/kumpmati/mvkump.dev.git",
+    repo: "https://github.com/kumpmati/kumpmati.github.io.git",
     user: {
       name: "kumpmati",
       email: "kumpulainen.matias@gmail.com",
--- src/server.js
+++ src/server.js
@@ -6,11 +6,9 @@ import * as sapper from "@sapper/server";
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development";
 
-const url = dev ? "/" : "mvkump.dev"; // <<-
-
 polka() // You can also use Express
   .use(
-    url,
+    "/",
     compression({ threshold: 0 }),
     sirv("static", { dev }),
     sapper.middleware()
